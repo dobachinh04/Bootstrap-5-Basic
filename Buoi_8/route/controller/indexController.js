ca6dabf4eb3647848f54c733183eb586766ad811
DoBaChinh <dobachinh04@gmail.com>
2024-03-21 21:48:30 +0700

    Add Layout - ng-view

---
+++ Buoi_8/route/controller/indexController.js
@@ -0,0 +1,12 @@
+window.listStudentController = function ($scope) {
+  $scope.title = "Danh Sách Sinh Viên";
+
+  $scope.listStudent = [
+    {
+      name: "Do Ba Chinh",
+      studentID: "PH32744",
+      from: "Ha Noi",
+      gender: "1",
+    },
+  ];
+};
