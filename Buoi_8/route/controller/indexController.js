e23e09ec43e9b1e8d3f1c5debab1c65b4e99d349
DoBaChinh <dobachinh04@gmail.com>
2024-03-23 18:06:11 +0700

    Delete Function

--- Buoi_8/route/controller/indexController.js
+++ Buoi_8/route/controller/indexController.js
@@ -1,12 +1,33 @@
-window.listStudentController = function ($scope) {
+window.listStudentController = function ($scope, $http) {
   $scope.title = "Danh Sách Sinh Viên";
 
-  $scope.listStudent = [
-    {
-      name: "Do Ba Chinh",
-      studentID: "PH32744",
-      from: "Ha Noi",
-      gender: "1",
-    },
-  ];
+  // $scope.listStudent = [
+  //   {
+  //     name: "Do Ba Chinh",
+  //     studentID: "PH32744",
+  //     from: "Ha Noi",
+  //     gender: "1",
+  //   },
+  // ];
+
+  let apiStudent = "http://localhost:3000/students";
+
+  // Sử dụng HTTP để gọi phương thức truy cập
+  $http.get(apiStudent).then(function (response) {
+    console.log(response);
+    if (response.status == 200) {
+      $scope.listStudent = response.data;
+    }
+  });
+
+  $scope.xoaSinhVien = function (idSinhVien) {
+    let confirm = window.confirm("Bạn có chắc là muốn xóa không?");
+    if (confirm) {
+      $http.delete(`${apiStudent}/${idSinhVien}`).then(function (response) {
+        if (response.status == 200) {
+          alert("Xóa thành công!");
+        }
+      });
+    }
+  };
 };
